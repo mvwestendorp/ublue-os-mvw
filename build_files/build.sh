b9d7491773bf9df6b1a1153dbf04ba74096b2128
Mathijs van Westendorp <mathijs@vanwestendorp.org>
2026-02-05 12:31:50 +0100

    gnuradio osmosdr is not a package for fedora 43

--- build_files/build.sh
+++ build_files/build.sh
@@ -93,7 +93,7 @@ dnf5 install -y distrobox buildah skopeo
 dnf5 install -y git git-lfs direnv fzf ripgrep fd-find jq
 
 # Install gnu radio
-dnf5 install -y gnuradio gnuradio-osmosdr
+dnf5 install -y gnuradio
 
 # Set environment variables for GNU Radio
 echo 'export PYTHONPATH=/usr/lib64/python3/site-packages:$PYTHONPATH' >> /etc/profile.d/gnuradio.sh
