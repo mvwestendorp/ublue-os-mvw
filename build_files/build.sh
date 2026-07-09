75da61b7e462a4be7c7027a6d1261b589e7d8192
Mathijs <mathijs@vanwestendorp.org>
2026-07-09 12:00:46 +0200

    Add virtualization packages installation to build.sh
    Added installation of virtualization packages to the build script.

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,6 +11,9 @@ set -ouex pipefail
 # Basic utilities
 dnf5 install -y tmux hunspell-nl pandoc
 
+# Install virtualization packages
+dnf5 install -y @virtualization
+
 # Install vscode
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo
